f20c6a4261f036f5dfce7faa373d12ba5e987142
F. Nicolas Medioli <mediolifnicolas@gmail.com>
2023-02-10 03:27:10 -0300

    changes in project structure to fix building errors

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -13,11 +13,11 @@ export const MainImage = () =>
                         <h2>No pierdas tiempo, compra tu entrada online en segundos!</h2>
 
                         <div className="emoji-container">
-                            <Image src="/../../assets/emojis/bailarina.png" alt="emoji emotion" />
-                            <Image src="/../../assets/emojis/bailarin.png" alt="emoji emotion" />
-                            <Image src="/../../assets/emojis/teatro.png" alt="emoji emotion" />
-                            <Image src="/../../assets/emojis/cerveza.png" alt="emoji emotion" />
-                            <Image src="/../../assets/emojis/anteojos.png" alt="emoji emotion" />
+                            <Image src="/assets/emojis/bailarina.png" alt="emoji emotion" />
+                            <Image src="/assets/emojis/bailarin.png" alt="emoji emotion" />
+                            <Image src="/assets/emojis/teatro.png" alt="emoji emotion" />
+                            <Image src="/assets/emojis/cerveza.png" alt="emoji emotion" />
+                            <Image src="/assets/emojis/anteojos.png" alt="emoji emotion" />
                         </div>
 
                         <button className="btn btn-info" onClick={
