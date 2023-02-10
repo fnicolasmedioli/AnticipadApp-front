3439fd4dac907946cf7b047a6c9d1a0bbedd622b
RamiroRojas3 <rojasramiro1999@gmail.com>
2023-02-09 21:37:10 -0300

    example of a home page

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -1,15 +1,26 @@
-
+import Image from "react-bootstrap/Image";
 
 export const MainImage = () =>
 {
 
     return (
         <div className="background-image">
-            <div>
-                <h1>AnticipadApp</h1>
-                <h2>Conseguí tu tickets online en segundos</h2>
+            <div className="text-centered">
+                <h1>La entrada perfecta para una noche perfecta</h1>
+                <h2>No pierdas tiempo, compra tu entrada online en segundos!</h2>
+
+                <div className="emoji-container">
+                    <Image src="/../../assets/emojis/bailarina.png" alt="emoji emotion" />
+                    <Image src="/../../assets/emojis/bailarin.png" alt="emoji emotion" />
+                    <Image src="/../../assets/emojis/teatro.png" alt="emoji emotion" />
+                    <Image src="/../../assets/emojis/cerveza.png" alt="emoji emotion" />
+                    <Image src="/../../assets/emojis/anteojos.png" alt="emoji emotion" />
+                </div>
+
                 <button className="btn btn-info">Próximos eventos</button>
+
             </div>
+            
         </div>
     );
 
