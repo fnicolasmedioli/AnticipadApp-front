0088760620b530c1d14da6700b69f50cb79097b4
F. Nicolas Medioli <mediolifnicolas@gmail.com>
2023-02-18 15:35:23 -0300

    +signup page and login improvements

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,51 +1,56 @@
 import { LoginBox } from "../auth/components";
 import { GlobalHeader } from "../global/components";
 import { GenericTemplate } from "./templates";
+import { Link as RouterLink } from "react-router-dom";
 
 export const LoginPage = () =>
 {
     return (
-        <div className="form-container">
-            <div className="form">
-                <h2>AnticipadApp</h2>
-                <h1>Bienvenido!</h1>
-                <p>Ingresa para continuar con su compra</p>
-                <div className="input">
-                    <div className="inputBox">
-                        <label for="">Email</label>
-                        <input type="email" />
+        <GenericTemplate>
+            <div className="form-container">
+                <div className="form">                
+                    <h1>Bienvenido!</h1>
+                    <p>Ingresa para continuar con la compra</p>
+                    <div className="input">
+                        <div className="inputBox">
+                            <label htmlFor="email-input">Email</label>
+                            <input type="email" id="email-input" />
+                        </div>
+                        <div className="inputBox">
+                            <label htmlFor="password-input">Contraseña</label>
+                            <input type="password" id="password-input" />
+                        </div>
+                        <div className="inputBox">
+                            <input type="submit" name="" value="Iniciar Sesión" className="pretty-button"/> 
+                        </div>
                     </div>
-                    <div className="inputBox">
-                        <label for="">Contraseña</label>
-                        <input type="password" />
-                    </div>
-                    <div className="inputBox">
-                        <input type="submit" name="" value="Iniciar Sesión" /> 
+                    <p className="forgot"><a href="#">Olvidaste tu Contraseña?</a></p>
+
+                    <div className="alternativeLogin">
+                        <label>Sino ingresá con:</label>
+                        <div className="iconGroup">
+                            <a href="#" className="btn icon-details">
+                                <i className="fab fa-facebook" aria-hidden="true"></i> Facebook
+                            </a>
+
+                            <a href="#" className="btn icon-details">
+                                <i className="fab fa-twitter" aria-hidden="true"></i> Twitter
+                            </a>
+
+                            <a href="#" className="btn icon-details">
+                                <i className="fab fa-google" aria-hidden="true"></i> Google
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <p className="forgot"><a href="#">Olvidaste tu Contraseña?</a></p>
 
-                <div className="alternativeLogin">
-                    <label>Sino ingresá con:</label>
-                    <div className="iconGroup">
-                        <a href="#" target="_blank" class="btn icon-details">
-                            <i class="fab fa-facebook" aria-hidden="true"></i> Facebook
-                        </a>
-
-                        <a href="#" target="_blank" class="btn icon-details">
-                            <i class="fab fa-twitter" aria-hidden="true"></i> Twitter
-                        </a>
-
-                        <a href="#" target="_blank" class="btn icon-details">
-                            <i class="fab fa-google" aria-hidden="true"></i> Google
-                        </a>
-
-                    </div>
-                </div>
-                
+                <p className="forgot" style={{
+                    marginTop: "2rem"
+                }}>
+                    Todavía no estás registrado?&nbsp;
+                    <RouterLink to="/signup">Registrate acá</RouterLink>
+                </p>
             </div>
-
-        </div>
-        
+        </GenericTemplate>
     );
 };
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,12 +1,43 @@
+import { LoginBox } from "../auth/components";
+import { GlobalHeader } from "../global/components";
 import { GenericTemplate } from "./templates";
+import { Link as RouterLink } from "react-router-dom";
 
 export const SignupPage = () =>
 {
     return (
-    <GenericTemplate>
-        <h4 style={{
-            color: "white"
-        }}>Signup box</h4>
-    </GenericTemplate>
+        <GenericTemplate>
+            <div className="form-container">
+                <div className="form">                
+                    <h1>Registro</h1>
+                    
+                    <div className="input">
+                        <div className="inputBox">
+                            <label htmlFor="email-input">Email</label>
+                            <input type="email" id="email-input" />
+                        </div>
+                        <div className="inputBox">
+                            <label htmlFor="password-input">Contraseña</label>
+                            <input type="password" id="password-input" />
+                        </div>
+                        <div className="inputBox">
+                            <label htmlFor="password-input-2">Repite la contraseña</label>
+                            <input type="password" id="password-input-2" />
+                        </div>
+                        <div className="inputBox">
+                            <input type="submit" name="" value="Registrarme" className="pretty-button"/> 
+                        </div>
+                    </div>
+                
+                </div>
+
+                <p className="forgot" style={{
+                    marginTop: "2rem"
+                }}>
+                    Ya estás registrado?&nbsp;
+                    <RouterLink to="/login">Ingresá acá</RouterLink>
+                </p>
+            </div>
+        </GenericTemplate>
     );
 };
