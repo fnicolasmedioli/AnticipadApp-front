87cae1a88e9b1745280ab6ee2808cff5b22a8fb2
F. Nicolas Medioli <mediolifnicolas@gmail.com>
2023-02-20 23:10:14 -0300

    +location box interactivity

--- src/components/LocationBox.jsx
+++ src/components/LocationBox.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 
 import { UserContext } from "../context";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
@@ -7,6 +7,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const LocationBox = () => {
     const { userData } = useContext(UserContext);
     const [inputValue, setInputValue] = useState("");
+
+    const inputRef = useRef();
+    const resultsRef = useRef();
 
     // Boolean to manage input content when location is detected
     const [firstValueSettled, settleFirstValue] = useState(false);
@@ -24,6 +27,14 @@ export const LocationBox = () => {
         setInputValue(target.value);
     }
 
+    function showResults() {
+        resultsRef.current.classList.remove("hidden");
+    }
+
+    function hideResults() {
+        resultsRef.current.classList.add("hidden");
+    }
+
     return (
         <div className="location-box">
             <span>Estás en </span>
@@ -34,13 +45,20 @@ export const LocationBox = () => {
                     name="search-place-input"
                     placeholder="Ingresa tu localidad..."
                     onChange={onInputChange}
+                    onFocus={showResults}
+                    onBlur={hideResults}
+                    ref={inputRef}
                 />
-                <span>
+                <span
+                    onClick={() => {
+                        inputRef.current.select();
+                    }}
+                >
                     <FontAwesomeIcon icon={faChevronDown} />
                 </span>
             </div>
 
-            <div className="search-results-box hidden">
+            <div className="search-results-box hidden" ref={resultsRef}>
                 <div className="search-result">Rauch, Buenos Aires</div>
                 <div className="search-result">Tandil, Buenos Aires</div>
             </div>
