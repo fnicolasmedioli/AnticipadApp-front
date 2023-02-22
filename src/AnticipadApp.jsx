0e446d5bb881414363373c2f4b2d9caac4fb2616
F. Nicolas Medioli <mediolifnicolas@gmail.com>
2023-02-22 11:49:59 -0300

    improved code quality

--- src/AnticipadApp.jsx
+++ src/AnticipadApp.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "animate.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
 import "./style/global-style.scss";
 import "./style/auth-style.scss";
@@ -11,37 +9,9 @@ import { MainRouter } from "./Router";
 import { UserProvider, GeoProvider } from "./context";
 
 const AnticipadApp = () => {
-    const [userData, setUserData] = useState({});
-
-    useEffect(() => {
-        axios
-            .request({
-                method: "get",
-                url: "https://ipapi.co/json",
-            })
-            .then(response => {
-                const location = {
-                    city: response.data.city,
-                    region: response.data.region,
-                };
-                console.log("Location: ");
-                console.log(location);
-                setUserData({
-                    ...userData,
-                    location,
-                });
-            })
-            .catch(console.error);
-    }, []);
-
     return (
         <BrowserRouter>
-            <UserProvider
-                value={{
-                    userData,
-                    setUserData,
-                }}
-            >
+            <UserProvider>
                 <GeoProvider>
                     <MainRouter />
                 </GeoProvider>
--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import { UserContext } from ".";
 
-export const UserProvider = ({ children, value }) => {
+export const UserProvider = ({ children }) => {
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        axios
+            .request({
+                method: "get",
+                url: "https://ipapi.co/json",
+            })
+            .then(response => {
+                const location = {
+                    city: response.data.city,
+                    region: response.data.region,
+                };
+                console.log("Location: ");
+                console.log(location);
+                setUserData({
+                    ...userData,
+                    location,
+                });
+            })
+            .catch(console.error);
+    }, []);
+
     return (
-        <UserContext.Provider value={value}>{children}</UserContext.Provider>
+        <UserContext.Provider
+            value={{
+                userData,
+                setUserData,
+            }}
+        >
+            {children}
+        </UserContext.Provider>
     );
 };
